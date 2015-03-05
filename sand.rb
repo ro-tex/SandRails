41ed5ffacba1c96e9439729518b19e6d1b9d36b5
Ivaylo Novakov <inovakov@gmail.com>
2015-03-05 18:35:16 +0200

    Learned to create classes, constructors, getters and setters.

---
+++ sand.rb
@@ -0,0 +1,43 @@
+class Greeter
+
+	attr_accessor :name
+
+	# constructor
+	def initialize(name = "World")
+		self.name = name
+	end
+
+	# getter
+	def name
+		results = "--> " + @name + " <--"
+	end
+
+	#setter
+	def name=(name)
+		@name = name.capitalize
+	end
+
+	# static
+	def self.whoami
+		puts "I am Groot!"
+	end
+
+	def hi
+		puts "Hello #{@name.capitalize}"
+	end
+
+	def bye
+		puts "Bye #{@name.capitalize}"
+	end
+
+end
+
+g = Greeter.new "Andy"
+
+#g.hi
+#g.class.whoami
+#puts Greeter.instance_methods
+#puts Greeter.to_s
+#g.name = "a"
+
+puts g.name
