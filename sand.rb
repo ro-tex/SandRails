617853888de82e148d3f0280516c27dce6f0d0b7
Ivaylo Novakov <inovakov@gmail.com>
2015-03-06 16:58:11 +0200

    Loops, duck typing, respond_to, trailing if, sort of 'main' (if __FILE__ == $0).

--- sand.rb
+++ sand.rb
@@ -37,12 +37,36 @@ class Greeter
 
 end
 
-g = Greeter.new("Andy", "Rudy", "Seth")
+# This allows a file to be used as a library, and not to execute code in that context,
+# but if the file is being used as an executable, then execute that code.
+if __FILE__ == $0
 
-#g.hi
-#g.class.whoami
-#puts Greeter.instance_methods
-#puts Greeter.to_s
-#g.name = "a"
+	#g = Greeter.new "Andy"
+	#g.hi
+	#g.class.whoami
+	#g.name = "a"
+	#puts g.name
 
-puts g.name
+	puts ">>> Playing around with arrays and loops <<<"
+	arr = ["a", "b", "c"]
+
+	if arr.respond_to?("each") # THIS CHECKS IF WE CAN ITERATE OVER THE COLLECTION WITH EACH! OMFG!
+		arr.each do |e|
+			puts e + " in a loop"
+		end
+	else
+		puts "arr is not each-able. This is what it looks like: " + arr.to_s
+	end
+
+	puts "This is a joined list: #{arr.join(", ")}" # notice the quotes in quotes!
+
+	puts "Duck typing: #{arr}"
+
+	arr = nil
+	puts "arr is nil" if arr.nil? # trailing if demo
+	puts !arr.nil? && arr.length > 0 ? ("arr len is " + arr.length.to_s) : "empty" # shorthand if works
+
+end
+
+
+
