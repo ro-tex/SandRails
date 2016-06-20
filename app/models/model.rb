a50e704c80559d053526e546c27e07be72413ca9
Ivaylo Novakov <inovakov@gmail.com>
2016-06-20 16:08:04 +0300

    Delayed job.

---
+++ app/models/model.rb
@@ -0,0 +1,17 @@
+class Model
+
+  def slow
+    sleep 2
+    puts 'hi'
+  end
+  handle_asynchronously :slow, run_at: Proc.new { 1.minute.from_now }
+ 
+  # Run like this:
+  # Delayed::Worker.new.run(Delayed::Job.find(id))
+  # Delayed::Worker.new.run(Delayed::Job.last)
+
+  def quick
+  	puts 'oi!'
+  end
+
+end
