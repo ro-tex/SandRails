297c237c2dd26dc5e2ffed340189e8940bdbb2d7
Ivaylo Novakov <inovakov@gmail.com>
2016-08-14 00:23:28 +0300

    More fun with singleton methods.

--- playground/modules.rb
+++ playground/modules.rb
@@ -16,8 +16,13 @@ mo.print_name
 # On the fly object rewriting! Mind = blown! Hadn't seen this fucked up shit since Perl days.
 class << mo
   def print_name
-    puts "I'm not Leopold! Ha!"
+    puts "I'm not #{NAME}! Ha! (overloading the method within the Singleton class)"
   end
 end
 
+def mo.print_real_name
+  puts 'Pinky and no(t) Brain! (Singleton method)'
+end
+
 mo.print_name
+mo.print_real_name
