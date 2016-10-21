a69ac3a772661270ac81f424eb1a945c66c4e159
ro-tex <inovakov@gmail.com>
2016-10-22 02:03:29 +0300

    Eigenclass, singleton methods, callbacks. Adding methods on the go and then removing them.

---
+++ playground/eigenclass.rb
@@ -0,0 +1,78 @@
+# http://www.integralist.co.uk/posts/eigenclass.html
+# https://www.sitepoint.com/premium/screencasts/learning-about-the-uses-of-singleton-methods-in-ruby
+
+# We'll reopen String and add some nice callbacks!
+# Those callbacks are active for the scope where we define them:
+# 1. the class: those will trigger when we add a singleton method to instances of the class
+# 2. the class's own singleton object: then those will trigger when we add singleton methods
+#   to the class itself. Note: class methods *are* singleton methods.
+class String
+  class << self
+    # These below will only be active for the eigenclass object
+    def singleton_method_added(id)
+      puts "String class >> Adding #{id.id2name}..."
+    end
+
+    def singleton_method_removed(id)
+      puts "String class >> Removing #{id.id2name}..."
+    end
+
+    def singleton_method_undefined(id)
+      puts "String class >> Undef-ing #{id.id2name}..."
+    end
+  end
+
+  # These will be active for instances of the class
+  def singleton_method_added(id)
+    puts "String object ## Adding #{id.id2name}..."
+  end
+
+  def singleton_method_removed(id)
+    puts "String object ## Removing #{id.id2name}..."
+  end
+
+  def singleton_method_undefined(id)
+    puts "String object ## Undef-ing #{id.id2name}..."
+  end
+end
+
+class String
+  def imp
+    self + '!'
+  end
+
+  # singleton for the class's eigenclass object
+  def self.o
+    'Ooh!'
+  end
+end
+
+s = 'someting'
+r = 'something else'
+puts s.imp
+puts r.imp
+
+# This is a singleton method - it only exists for this one object
+def s.very_imp
+  self + '!!!'
+end
+
+puts s.very_imp
+# puts r.very_imp # -> Error: undefined method `very_imp' for "something else":String (NoMethodError)
+
+# This is the way to find all singleton methods for an object
+puts s.singleton_methods.inspect
+puts String.singleton_methods.inspect
+
+# this is how we de-register a class singleton
+class String
+  class <<self
+    remove_method :o
+  end
+end
+
+# another way to execute something in the context of a class is this:
+String.class_exec { remove_method :imp }
+
+# and this is how we de-register an object's singleton
+s.instance_eval { undef :very_imp }
