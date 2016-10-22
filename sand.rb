847178d3ff355ffbc658d85bdc32bd6a73c50660
ro-tex <inovakov@gmail.com>
2016-10-23 00:39:32 +0300

    This is interesting because it shows the optimisations the beautifier makes.

--- sand.rb
+++ sand.rb
@@ -1,5 +1,5 @@
 # if you want to turn this into a script:
-#!/usr/bin/env ruby
+# !/usr/bin/env ruby
 # or this: #!/usr/bin/ruby
 
 $LOAD_PATH << '.' # tells Ruby to look for files in the current dir
@@ -7,7 +7,6 @@ $LOAD_PATH << '.' # tells Ruby to look for files in the current dir
 # require 'lib'
 
 class Greeter
-
   # define default getter and setter, just like making the field public
   # if you define custom getters they will override the defaults, ofc.
   attr_accessor :name
@@ -17,29 +16,29 @@ class Greeter
   @instance_variable = 'instance'
   @@class_variable = 'class'
   $global_variable = 'global'
-  CONSTANT = 'constant' # but only as a convention
+  CONSTANT = 'constant'.freeze # but only as a convention
 
-  #How do you define a class variable?
-  @@my_name = "Groot"
+  # How do you define a class variable?
+  @@my_name = 'Groot'
 
   # constructor
-  def initialize(name = "World")
+  def initialize(name = 'World')
     @name = name.capitalize
   end
 
   # getter
   def name
-    results = "--> " + @name + " <--"
+    results = '--> ' + @name + ' <--'
   end
 
-  #setter
+  # setter
   def name=(name)
     @name = name.capitalize
   end
 
   # static
   def self.whoami
-    puts "I am " + @@my_name + "!"
+    puts 'I am ' + @@my_name + '!'
   end
 
   def hi
@@ -53,66 +52,63 @@ class Greeter
   def self.foo
     puts Lib.foo
   end
-
 end
 
-#Greeter.all
-#Greeter.where(name: 'Groot', instance_variable: 'instance')
+# Greeter.all
+# Greeter.where(name: 'Groot', instance_variable: 'instance')
 
-#Greeter.foo # this is how we use libraries
+# Greeter.foo # this is how we use libraries
 
 # This allows a file to be used as a library, and not to execute code in that context,
 # but if the file is being used as an executable, then execute that code.
-if __FILE__ == $0
-  #puts 'Our sand.rb is being used directly - as an executable file.'
+if __FILE__ == $PROGRAM_NAME
+  # puts 'Our sand.rb is being used directly - as an executable file.'
 end
 
-g = Greeter.new "Andy"
-#g.hi
-#g.class.whoami
-#g.name = "a"
-#puts g.name
+g = Greeter.new 'Andy'
+# g.hi
+# g.class.whoami
+# g.name = "a"
+# puts g.name
 
 if false # basics
-  arr = ["a", "b", "c"]
+  arr = %w(a b c)
 
-  if arr.respond_to?("each") # THIS CHECKS IF WE CAN ITERATE OVER THE COLLECTION WITH EACH! OMFG!
+  if arr.respond_to?('each') # THIS CHECKS IF WE CAN ITERATE OVER THE COLLECTION WITH EACH! OMFG!
     arr.each do |e|
-      puts e + " in a loop"
+      puts e + ' in a loop'
     end
   else
     puts "arr is not each-able. This is what it looks like: #{arr}"
   end
 
-  puts "This is a joined list: #{arr.join(", ")}" # notice the quotes in quotes!
+  puts "This is a joined list: #{arr.join(', ')}" # notice the quotes in quotes!
 
   puts "Duck typing: #{arr}"
 
   arr = nil
-  puts "arr is nil" if arr.nil? # trailing if demo
+  puts 'arr is nil' if arr.nil? # trailing if demo
   puts 'trailing unless' unless false
 
-  unless false
-    puts 'regular unless'
-  end
+  puts 'regular unless' unless false
 
-  puts !arr.nil? && arr.length > 0 ? ("arr len is " + arr.length.to_s) : "empty" # shorthand if works
+  puts !arr.nil? && !arr.empty? ? ('arr len is ' + arr.length.to_s) : 'empty' # shorthand if works
 
   (1...3).each { |a| puts a } # ranges that exclude the end value (use .. to include it):
-  
-  20.step(1, -3) {|x| puts x} # a fully crontolled for loop: from.step(to, step)
 
-  {"a" => 1, "b" => 3}.each { |k, v| puts "key #{k} -> value #{v}" } # hashes
+  20.step(1, -3) { |x| puts x } # a fully crontolled for loop: from.step(to, step)
 
-  arr = "asdf"
+  { 'a' => 1, 'b' => 3 }.each { |k, v| puts "key #{k} -> value #{v}" } # hashes
+
+  arr = 'asdf'
   # a nice, flexible switch... erm... case
   case arr
   when nil
-    puts "arr is nil!"
-    #when 1..5: puts "arr is one"
-  when "asdf", "asdfg" then puts arr
+    puts 'arr is nil!'
+    # when 1..5: puts "arr is one"
+  when 'asdf', 'asdfg' then puts arr
   else
-    puts "none of the above"
+    puts 'none of the above'
   end
 end # basics
 
@@ -121,9 +117,9 @@ if false # exceptions_and_control
   for i in 1..5
     # do nothing
     next if i == 2 # aka 'continue'
-    #next: Jumps to next iteration of the most internal loop. Terminates execution of a block if called within a block (with yield or call returning nil).
+    # next: Jumps to next iteration of the most internal loop. Terminates execution of a block if called within a block (with yield or call returning nil).
 
-    if $! == "bla" # if the last raised exception is 'bla' redo the innermost loop
+    if $ERROR_INFO == 'bla' # if the last raised exception is 'bla' redo the innermost loop
       redo # Restarts this iteration of the most internal loop, without checking loop condition. Restarts yield or call if called within a block.
     end
   end
@@ -131,35 +127,31 @@ if false # exceptions_and_control
   # try-catch-retry:
   begin
     # do_something # exception raised
-  rescue
-    # handles error - last exception is in $!
-    retry # restart from beginning - similar to redo
-  else
+    rescue
+        # handles error - last exception is in $!
+        retry # restart from beginning - similar to redo
+    else
     # this is only executed if no exceptions were raised in the main block
-  ensure
+    ensure
     # Ruby's 'finally'
   end
-  
-  throw :lablename condition
-    #.. this will not be executed
-  catch :lablename do
-    #.. matching catch will be executed after a throw is encountered.
-  end
 
-  for i in 1..5
-    retry if false # restart from i == 1
+  throw :lablename if true # true is the condition
+  # .. this will not be executed
+  catch :lablename do
+    # .. matching catch will be executed after a throw is encountered.
   end
 
 end # exceptions_and_control
 
 if false # multi-dimentional returns and processing
 
-  #define a method that returns a value
+  # define a method that returns a value
   def get_arr
     a1 = [1, 2, 3]
     a2 = [4, 5, 6]
     a3 = [7, 8, 9]
-    return [a1, a2, a3]
+    [a1, a2, a3]
   end
 
   # easy iteration over multidimensional data structures
@@ -169,7 +161,7 @@ if false # multi-dimentional returns and processing
 
 end
 
-defined? $_ #always true
+defined? $LAST_READ_LINE # always true
 
 n = 1_024 # underscores are ignored in integers
 
@@ -188,13 +180,16 @@ a multiline comment
 in Ruby.
 =end
 
-def tag_list= value # I guess a way to define a param?
+def tag_list=(value) # I guess a way to define a param?
   self.tags = value.split(',').map(&:strip) # why do we need the &? how does map work? what's :strip, exactly?
 end
 
 a ||= {} # conditional initialisation - nothing happens if already initialised
 
-def foo(*args) ap arr args end # variable number of arguments
+# variable number of arguments
+def foo(*args)
+  ap arr args
+end
 
 alias bar foo # pointer to a function. points to the original function even after foo is overridden
 
@@ -213,9 +208,9 @@ if true # Blocks
     yield
     puts ' === end === '
   end
-  block { |val| puts "Block with value #{val}"} # this line is the call, i.e. executes the function
+  block { |val| puts "Block with value #{val}" } # this line is the call, i.e. executes the function
 
-  def foo x
+  def foo(x)
     puts ' === foo === '
 
     unless block_given?
@@ -233,7 +228,7 @@ if true # Blocks
   end
   foo(1) # Try without giving a block
   foo(1) { |n| n += 10; puts "in da block: #{n}" } # Executes foo but can't change the local variable x
-  l = -> n { n += 20; puts "in da lambda: #{n}"}
+  l = -> (n) { n += 20; puts "in da lambda: #{n}" }
   foo(1, &l) # This is the most functional bit - passing a named lambda
 
 end
@@ -241,14 +236,14 @@ end
 if false # Modules
 
   module Mod1
-    CONST1 = 12345
+    CONST1 = 12_345
     def foo
       puts "Mod1 #{CONST1}"
     end
   end
 
   module Mod2
-    CONST2 = 54321
+    CONST2 = 54_321
     def foo
       puts "Mod2 #{CONST2}"
     end
@@ -271,34 +266,17 @@ if false
   # puts %q(this is a single-quoted string: #{1+2+3})
   # puts %Q(this is a double-quoted string: #{1+2+3})
   # puts %x{ls -lh} # this string is the output of the command
-  puts " lala   ".strip # remove whitespaces
-  puts "abcdefg"[2..5] # "cdef" - substring
-  puts "abcdefg"[2, 5] # "cdefg" - offset, length
-  puts "abcdefg"[-5, 2] # negative fixnum starts from the end
-  puts "abcdefg"[/bcd/] # substring by regex
+  puts ' lala   '.strip # remove whitespaces
+  puts 'abcdefg'[2..5] # "cdef" - substring
+  puts 'abcdefg'[2, 5] # "cdefg" - offset, length
+  puts 'abcdefg'[-5, 2] # negative fixnum starts from the end
+  puts 'abcdefg'[/bcd/] # substring by regex
 
-  "abc".is_a?(String) # typeof check
-  "abc".instance_of? String
-  "abc".respond_to?(:to_s) # Duck typing, if it qucks like a string then it's a string
+  'abc'.is_a?(String) # typeof check
+  'abc'.instance_of? String
+  'abc'.respond_to?(:to_s) # Duck typing, if it qucks like a string then it's a string
 
-  "abc".inspect # Returns a printable version of str, with special characters escaped.
-  "abc".intern === "abc".to_sym # Returns the Symbol corresponding to str, creating the symbol if it did not previously exist.
-  "abc".scan('a') {|match| puts match}
+  'abc'.inspect # Returns a printable version of str, with special characters escaped.
+  'abc'.intern === 'abc'.to_sym # Returns the Symbol corresponding to str, creating the symbol if it did not previously exist.
+  'abc'.scan('a') { |match| puts match }
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
