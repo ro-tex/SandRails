de01092d4e16beaca133498a0a8b03f3857aaee8
Ivaylo Novakov <inovakov@gmail.com>
2016-12-15 16:01:35 +0200

    Label different runs.

--- playground/benchmark.rb
+++ playground/benchmark.rb
@@ -7,14 +7,14 @@ iterations = 100_000
 
 Benchmark.bm do |bm|
   # joining an array of strings
-  bm.report do
+  bm.report('joining an array of strings') do
     iterations.times do
       ['The', 'current', 'time', 'is', Time.now].join(' ')
     end
   end
 
   # using string interpolation
-  bm.report do
+  bm.report('string interpolation') do
     iterations.times do
       "The current time is #{Time.now}"
     end
