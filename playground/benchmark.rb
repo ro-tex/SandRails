d5c1fbce9f370b228904a3bffb9d1d900efeee08
Ivaylo Novakov <inovakov@gmail.com>
2016-12-16 15:35:00 +0200

    How to avoid bias caused by memory allocation, GC and so on.

--- playground/benchmark.rb
+++ playground/benchmark.rb
@@ -1,11 +1,14 @@
 require 'benchmark'
 require 'bigdecimal/math'
 
-# puts Benchmark.measure { BigMath.PI(10_000) }
+# Measure a single block of code
+puts Benchmark.measure { BigMath.PI(10_000) }
+puts "\n"
 
 iterations = 100_000
 padding = 30
 
+# Measure and compare different approaches
 Benchmark.bm(padding) do |bm|
   # joining an array of strings
   bm.report('joining an array of strings') do
@@ -19,5 +22,23 @@ Benchmark.bm(padding) do |bm|
     iterations.times do
       "The current time is #{Time.now}"
     end
+  end
+end
+
+puts "\n\n"
+
+array = Array(1..10_000_000)
+
+# The method name is suitable because it actually benchmarks your blocks of code twice.
+# First, it runs the code as a ‘rehearsal to force any initialization that needs to happen,
+# then it forces the GC to run, then it runs the benchmark again ‘for real. This ensures
+# that the system is fully initialized and the benchmark is fair.
+Benchmark.bmbm(7) do |bm|
+  bm.report('reverse') do
+    array.dup.reverse
+  end
+
+  bm.report('reverse!') do
+    array.dup.reverse!
   end
 end
