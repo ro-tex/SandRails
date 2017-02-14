2115acda43809807e9f1e8db7376daf492452065
Ivaylo Novakov <inovakov@gmail.com>
2017-02-14 13:07:08 +0200

    Added posts to the blog - model, views, forms, etc.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829122939) do
+ActiveRecord::Schema.define(version: 20170213181954) do
 
   create_table "models", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "field"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
